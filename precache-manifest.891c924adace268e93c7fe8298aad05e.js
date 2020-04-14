517e03ae98fa7e8badacdb7010f0f7f675662c5e
osujhoanny <37456363+osujhoanny@users.noreply.github.com>
2020-04-13 20:34:21 -0500

    Updates

---
+++ precache-manifest.891c924adace268e93c7fe8298aad05e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132b4e8bbcd791c45c298090adb6fb35",
+    "url": "/buildTest/index.html"
+  },
+  {
+    "revision": "8b89dc6a6a5e348dc4ed",
+    "url": "/buildTest/static/css/main.b241c7a4.chunk.css"
+  },
+  {
+    "revision": "75733418c299cc7d35da",
+    "url": "/buildTest/static/js/2.da2cc25a.chunk.js"
+  },
+  {
+    "revision": "2d76e6dd90213504b48ff703fdec8554",
+    "url": "/buildTest/static/js/2.da2cc25a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b89dc6a6a5e348dc4ed",
+    "url": "/buildTest/static/js/main.cc381fa0.chunk.js"
+  },
+  {
+    "revision": "37b280c699832f41b696",
+    "url": "/buildTest/static/js/runtime-main.b6fafcae.js"
+  },
+  {
+    "revision": "0b905b27a06eaf4d477bfb163889f6c4",
+    "url": "/buildTest/static/media/login.0b905b27.svg"
+  },
+  {
+    "revision": "979bfce60b2946711f2551e85c233df7",
+    "url": "/buildTest/static/media/logo.979bfce6.svg"
+  },
+  {
+    "revision": "8f5af410e1454393f183176f08bc12cf",
+    "url": "/buildTest/static/media/register.8f5af410.svg"
+  }
+]);
